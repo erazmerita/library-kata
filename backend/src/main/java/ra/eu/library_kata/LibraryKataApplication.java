db6abd5d05654a4bcd384b9f490aeb4a4f42a9b1
Eugeniu Razmerita <raeu92@gmail.com>
2025-01-30 15:41:44 +0200

    Add backend initial project

---
+++ backend/src/main/java/ra/eu/library_kata/LibraryKataApplication.java
@@ -0,0 +1,13 @@
+package ra.eu.library_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryKataApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ra/eu/library_kata/LibraryKataApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.eu.library_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
